c2d53bf7540ff21b5e25f2268e27e7dced310d50
MierafA12 <mierafabebe12@gmail.com>
2025-07-27 12:33:48 +0300

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Your App',
+      debugShowCheckedModeBanner: false,
       home: OnboardingScreen(),
     );
   }
